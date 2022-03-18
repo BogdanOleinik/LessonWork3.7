10519f88ae0045126f21f5d9e170aa1c617b935e
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-03-18 13:59:56 +0300

    Initial Commit

---
+++ LessonWork3.7/LessonWork3_7App.swift
@@ -0,0 +1,17 @@
+//
+//  LessonWork3_7App.swift
+//  LessonWork3.7
+//
+//  Created by Олейник Богдан on 18.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LessonWork3_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
