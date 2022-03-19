b5c19c735477d5a605aa1a3f89f3be5bf5905bd7
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-03-19 12:41:43 +0300

    1.1

--- LessonWork3.7/CustonGridView.swift
+++ LessonWork3.7/CustonGridView.swift
@@ -11,22 +11,22 @@ struct CustonGridView<Content, T>: View where Content: View {
     
     let items: [T]
     let colums: Int
-    let content: (T) -> Content
+    let content: (CGFloat, T) -> Content
     var rows: Int {
         items.count / colums
     }
     
     var body: some View {
         GeometryReader { geometry in
-            let sideLength = geometry.size.width / CGFloat(colums)
+            let sideSize = geometry.size.width / CGFloat(colums)
             ScrollView {
                 VStack {
                     ForEach(0...rows, id: \.self) { rowIndex in
                         HStack {
                             ForEach(0..<colums) { columnIndex in
                                 if let index = indexFor(row: rowIndex, column: columnIndex) {
-                                    content(items[index])
-                                        .frame(width: sideLength, height: sideLength)
+                                    content(sideSize, items[index])
+                                        .frame(width: sideSize, height: sideSize)
                                 } else {
                                     Spacer()
                                 }
@@ -46,8 +46,9 @@ struct CustonGridView<Content, T>: View where Content: View {
 
 struct CustonGridView_Previews: PreviewProvider {
     static var previews: some View {
-        CustonGridView(items: [21, 2, 6, 13, 5, 6, 10], colums: 3) { item in
+        CustonGridView(items: [21, 2, 6, 13, 5, 6, 10], colums: 3) { sideSize, item in
             Text("\(item)")
+                .frame(width: sideSize, height: sideSize)
         }
     }
 }
